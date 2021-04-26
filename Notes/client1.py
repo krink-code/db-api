9b362f29e73694df75dc47342daf184f4fa15499
Karl <karl.rink@nationsinfocorp.com>
2021-04-26 15:53:59 -0700

    20210426-2

---
+++ Notes/client1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import requests
+
+username='dbapi'
+password='dbapi'
+
+response = requests.get('http://127.0.0.1:8980/api',
+                        auth=requests.auth.HTTPBasicAuth(
+                            username, password))
+print(response.text)
+
+
