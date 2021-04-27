abd75f2445631370b2d87200debebf552a52e3e3
Karl <karl.rink@nationsinfocorp.com>
2021-04-26 17:02:03 -0700

    20210426-3

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 # python3
 
-__version__='0.0.1'
+__version__='0.0.1.dev.20210426-1'
 
 from app import app
 from flask import request
@@ -56,8 +56,44 @@ def api(db=None, table=None):
     if limit:
         SQL += ' LIMIT ' + str(limit)
 
-    rows = fetchall(SQL)
-    return jsonify(rows), 200
+    #rows = fetchall(SQL)
+
+    #return jsonify(rows), 200
+    #return jsonify([rows.to_json() for row in rows]), 200
+
+    username = request.authorization.username
+    password = request.authorization.password
+    dbhost   = request.headers.get('X-Host', '127.0.0.1')
+    dbport   = request.headers.get('X-Port', '3306')
+
+    app.config['MYSQL_DATABASE_USER'] = username
+    app.config['MYSQL_DATABASE_PASSWORD'] = password
+    app.config['MYSQL_DATABASE_HOST'] = dbhost
+    app.config['MYSQL_DATABASE_PORT'] = int(dbport)
+
+    conn = mysql.connect()
+    cur = conn.cursor()
+    cur.execute(SQL)
+    rows = cur.fetchall()
+    columns = [desc[0] for desc in cur.description]
+    cur.close()
+    conn.close()
+
+    result = []
+    for row in rows:
+        row = dict(zip(columns, row))
+        result.append(row)
+
+    #return jsonify(result), 200
+
+    import json
+    json_response=json.dumps(result)
+    response=Response(json_response,content_type='application/json; charset=utf-8')
+    response.headers.add('content-length',len(json_response))
+    response.status_code=200
+    return response, 200
+
+
 
 
 @app.errorhandler(404)
