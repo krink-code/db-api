2e96dbe03791565a7d934485e570dbc1e3b5d812
Karl <karl.rink@nationsinfocorp.com>
2021-04-28 17:12:07 -0700

    0.0.1.dev.20210428-3

--- Notes/client3.bash
+++ Notes/client3.bash
@@ -6,8 +6,18 @@ curl --user dbuser:dbpass \
      -X PATCH  \
      -H "Content-Type: application/json" \
      -H "Accept: application/json"  \
-     -d '{"description": "A PATCH update"}' \
+     -d '{"description": "A PATCH update2"}' \
     http://127.0.0.1:8980/api/example/table1/3
 
-
      #-d '{"name": "Krink","description": "A PATCH update"}' \
+
+     #if description is already "A PATCH update"... 465
+
+
+#🐰 karl.rink@Karl-MacBook-Pro Notes % ./client3.bash
+#{"message":"Created","status":201,"update":"Success"}
+#🐰 karl.rink@Karl-MacBook-Pro Notes % ./client3.bash
+#{"message":"Failed Update","status":465}
+#🐰 karl.rink@Karl-MacBook-Pro Notes %
+
+
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 
 # python3
 
-__version__='0.0.1.dev.20210428-2'
+__version__='0.0.1.dev.20210428-3'
 
 from app import app
 from flask import request
 from flask import jsonify
 from werkzeug.exceptions import HTTPException
-from werkzeug.exceptions import Unauthorized
+#from werkzeug.exceptions import Unauthorized
 from db import mysql
 import cryptography
 from functools import wraps
@@ -124,37 +124,38 @@ def post_insert(db=None, table=None):
     assert db == request.view_args['db']
     assert table == request.view_args['table']
 
-    app.config['MYSQL_DATABASE_USER'] = request.authorization.username
-    app.config['MYSQL_DATABASE_PASSWORD'] = request.authorization.password
-    app.config['MYSQL_DATABASE_HOST'] = request.headers.get('X-Host', '127.0.0.1')
-    app.config['MYSQL_DATABASE_PORT'] = int(request.headers.get('X-Port', '3306'))
-
 
     if not request.headers['Content-Type'] == 'application/json':
         return jsonify(status=412, errorType="Precondition Failed"), 412
 
     post = request.get_json()
 
-    print(str(type(post)))
-    print(len(post))
+    #print(str(type(post)))
+    #print(len(post))
 
     placeholders = ['%s'] * len(post)
 
     fields = ",".join([str(key) for key in post])
     places = ",".join([str(key) for key in placeholders])
 
-    print(fields)
-    print(places)
-
+    #print(fields)
+    #print(places)
 
     #records = ['Jane', 'Another description']
     records=[]
     for key in post:
         records.append(post[key])
+
     SQL = "INSERT INTO " +str(db)+"."+str(table)+" ("+str(fields)+") VALUES ("+str(places)+")"
 
-    print(SQL)
+    #print(SQL)
+    #insert = insertsql(SQL, records)
+    #if insert == True:
 
+    app.config['MYSQL_DATABASE_USER'] = request.authorization.username
+    app.config['MYSQL_DATABASE_PASSWORD'] = request.authorization.password
+    app.config['MYSQL_DATABASE_HOST'] = request.headers.get('X-Host', '127.0.0.1')
+    app.config['MYSQL_DATABASE_PORT'] = int(request.headers.get('X-Port', '3306'))
 
     conn = mysql.connect()
     cur = conn.cursor()
@@ -162,11 +163,59 @@ def post_insert(db=None, table=None):
     conn.commit()
     cur.close()
     conn.close()
+    
 
-    return jsonify(status=201, message="Created"), 201
+    if cur.rowcount == 0:
+        return jsonify(status=461, message="Failed Create"), 461
+    else:
+        return jsonify(status=201, message="Created"), 201
 
 
-
+##multi-valued key1=val1,key2=val2,etc update
+##PATCH  /api/<db>/<table>/:id         # Update row element by primary key
+#@app.route("/api/<db>/<table>/<key>", methods=['PATCH'])
+#def patch_one(db=None, table=None, key=None):
+#
+#    assert db == request.view_args['db']
+#    assert table == request.view_args['table']
+#    assert key == request.view_args['key']
+#
+#    if not request.headers['Content-Type'] == 'application/json':
+#        return jsonify(status=412, errorType="Precondition Failed"), 412
+#
+#    post = request.get_json()
+#
+#    columns=[]
+#    records=[]
+#    for _key in post:
+#        columns.append(_key+'=?')
+#        records.append(post[_key])
+#
+#    fields = ",".join([str(k) for k in columns])
+#
+#    SQL = "UPDATE "+str(db)+"."+str(table)+" SET "+str(fields)+" WHERE id='"+str(key)+"'"
+#    print(SQL)
+#
+#    #update = insertsql(SQL, values)
+#    #if update is True:
+#
+#    app.config['MYSQL_DATABASE_USER'] = request.authorization.username
+#    app.config['MYSQL_DATABASE_PASSWORD'] = request.authorization.password
+#    app.config['MYSQL_DATABASE_HOST'] = request.headers.get('X-Host', '127.0.0.1')
+#    app.config['MYSQL_DATABASE_PORT'] = int(request.headers.get('X-Port', '3306'))
+#
+#    conn = mysql.connect()
+#    cur = conn.cursor()
+#    cur.execute(SQL, records)
+#    conn.commit()
+#    cur.close()
+#    conn.close()
+#
+#    if cur.rowcount == 0:
+#        #return jsonify(status=204, message="No Content"), 204
+#        return jsonify(status=201, message="Created", update="Success"), 201
+#    else:
+#        return jsonify(status=465, message="Failed Update"), 465
 
 
 
@@ -226,6 +275,44 @@ def delete_one(db=None, table=None, key=None):
 #WHERE condition; 
 
 
+##PATCH  /api/<db>/<table>/:id         # Update row element by primary key (single key/val)
+#@app.route("/api/<db>/<table>/<key>", methods=['PATCH'])
+#def patch_one(db=None, table=None, key=None):
+#
+#    assert db == request.view_args['db']
+#    assert table == request.view_args['table']
+#    assert key == request.view_args['key']
+#
+#    if not request.headers['Content-Type'] == 'application/json':
+#        return jsonify(status=412, errorType="Precondition Failed"), 412
+#
+#    post = request.get_json()
+#
+#    if len(post) > 1:
+#        return jsonify(status=405, errorType="Method Not Allowed", errorMessage="Single Key-Value Only"), 405
+#
+#    #colmns=[]
+#    values=[]
+#    for _key in post:
+#        field = _key
+#        values.append(post[_key])
+#    #    colmns.append(_key+'=?')
+#    #fields = ",".join([str(k) for k in colmns])
+#
+#    SQL = "UPDATE "+str(db)+"."+str(table)+" SET "+str(field)+"=? WHERE id='"+str(key)+"'"
+#    print(SQL)
+#    print(values)
+#
+#    update = insertsql(SQL, values)
+#    #update = True
+#
+#
+#    if update is True:
+#        #return jsonify(status=204, message="No Content"), 204
+#        return jsonify(status=201, message="Created", update="Success"), 201
+#    else:
+#        return jsonify(status=465, message="Failed Update"), 465
+
 #PATCH  /api/<db>/<table>/:id         # Update row element by primary key (single key/val)
 @app.route("/api/<db>/<table>/<key>", methods=['PATCH'])
 def patch_one(db=None, table=None, key=None):
@@ -243,29 +330,44 @@ def patch_one(db=None, table=None, key=None):
         return jsonify(status=405, errorType="Method Not Allowed", errorMessage="Single Key-Value Only"), 405
 
     #colmns=[]
-    values=[]
+    #records=[]
+
     for _key in post:
         field = _key
-        values.append(post[_key])
+        value = post[_key]
+
     #    colmns.append(_key+'=?')
     #fields = ",".join([str(k) for k in colmns])
 
-    SQL = "UPDATE "+str(db)+"."+str(table)+" SET "+str(field)+"=? WHERE id='"+str(key)+"'"
-    print(SQL)
-    print(values)
+    #SQL = "UPDATE "+str(db)+"."+str(table)+" SET "+str(field)+"=? WHERE id='"+str(key)+"'"
 
-    update = insertsql(SQL, values)
+    SQL = "UPDATE "+str(db)+"."+str(table)+" SET "+str(field)+"='"+str(value)+"' WHERE id='"+str(key)+"'"
+
+    #print(SQL)
+    #print(records)
+
+    #update = insertsql(SQL, values)
     #update = True
 
 
-    if update is True:
-        #return jsonify(status=204, message="No Content"), 204
-        return jsonify(status=201, message="Created", update="Success"), 201
-    else:
+    app.config['MYSQL_DATABASE_USER'] = request.authorization.username
+    app.config['MYSQL_DATABASE_PASSWORD'] = request.authorization.password
+    app.config['MYSQL_DATABASE_HOST'] = request.headers.get('X-Host', '127.0.0.1')
+    app.config['MYSQL_DATABASE_PORT'] = int(request.headers.get('X-Port', '3306'))
+
+    conn = mysql.connect()
+    cur = conn.cursor()
+    cur.execute(SQL)
+    #cur.execute(SQL, records)
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    if cur.rowcount == 0:
         return jsonify(status=465, message="Failed Update"), 465
-
-
-
+    else:
+        return jsonify(status=201, message="Created", update="Success"), 201
+    #hmmm.  this is 465 when success but nothing to update when column data and update are the same.
 
 
 #PUT    /api/<db>/<table>             # Replaces existing row with new row
@@ -290,10 +392,18 @@ def handle_exception(e):
     if isinstance(e, HTTPException):
         return jsonify(status=e.code, errorType="HTTP Exception", errorMessage=str(e)), e.code
 
+    #if isinstance(e, OperationalError):
+    #    return jsonify(status=512, errorType="OperationalError", errorMessage=str(e)), 512
+    #print(type(e).__name__)
+    if type(e).__name__ == 'OperationalError':
+        return jsonify(status=512, errorType="OperationalError", errorMessage=str(e)), 512
+
     res = {'status': 500, 'errorType': 'Internal Server Error'}
     res['errorMessage'] = str(e)
     return jsonify(res), 500
 
+#<class 'pymysql.err.OperationalError'>
+#(1054, "Unknown column 'descriptionXXXXX' in 'field list'")
 
 
 
@@ -307,7 +417,7 @@ def Conf(f):
         #if not request.authorization:
         #    print('no request.authorization')
         #    return jsonify(status=401,errorType="Unauthorized",errorMessage="Requires BasicAuth"), 401
-        #return f(*args, **kwargs)
+        return f(*args, **kwargs)
     return decorated
 
 
@@ -337,12 +447,13 @@ def insertsql(sql, values):
     cur = conn.cursor()
     cur.execute(sql, values)
     conn.commit()
+    print(str(cur.rowcount))
+    if cur.rowcount == 0:
+        cur.close()
+        conn.close()
+        return False
     cur.close()
     conn.close()
-    #if cur.rowcount == 0:
-    #    return False
-    #else:
-    #    return True
     return True
 
 @Conf
