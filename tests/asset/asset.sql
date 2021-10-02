630c39636d63c5c3308d055b2e6b1267ba4e7687
Karl Rink <karl@rink.us>
2021-10-02 14:43:46 -0700

    20211002-0

---
+++ tests/asset/asset.sql
@@ -0,0 +1,20 @@
+
+-- mysql/mariadb
+
+CREATE DATABASE IF NOT EXISTS asset;
+
+CREATE TABLE IF NOT EXISTS asset.inventory (
+  sn VARCHAR(255) PRIMARY KEY NOT NULL,
+  name VARCHAR(255),
+  description LONGTEXT,
+  value DECIMAL(15,2),
+  picture LONGBLOB,
+  json JSON,
+  note TEXT,
+  created DATETIME DEFAULT CURRENT_TIMESTAMP,
+  modified DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  FULLTEXT(name, description, note)
+) ENGINE=InnoDB;
+
+
+
