dd2d33358f4f44ac3c4cfb2778f7bab6acd0e2cb
Karl Rink <karl@rink.us>
2021-10-04 09:40:30 -0700

    20211004-0

---
+++ tests/example2/insert.picture.py
@@ -0,0 +1,25 @@
+
+# -*- coding: utf-8 -*-
+
+import mysql.connector
+
+picture = 'image.jpg'
+
+with open(picture, 'rb') as f:
+    photo = f.read()
+
+
+#import base64
+#encodestring = base64.b64encode(photo)
+
+db = mysql.connector.connect(user="dbuser",password="dbpass",host="localhost",database="")
+
+cursor = db.cursor()
+#sql = "insert into sample values(%s)"
+sql = "update asset.inventory set picture = %s where sn='sn001'"
+#cursor.execute(sql,(encodestring,))
+cursor.execute(sql,(photo,))
+db.commit()
+db.close()
+
+
