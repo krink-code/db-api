535e31e319294dc54ce776ca0b65e9ef5ebb0fbb
Karl <karl.rink@nationsinfocorp.com>
2022-01-17 14:12:36 -0800

    clients/curl/dbapi.sh

--- clients/curl/dbapi.sh
+++ clients/curl/dbapi.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-sql(){
+dbapi(){
 curl --user dbuser:dbpass \
      -X POST \
      -H "Content-Type: text/sql" \
      --data "$1" \
-     "http://127.0.0.1:8980/api"
+     http://127.0.0.1:8980/api
 }
 
 usage(){
-  echo "Usage: $0 sql 'sql statement'"
+  echo "Usage: $0 'sql statement'"
 }
 
-case $1 in
-  sql) sql "$2";;
-  *)usage;;
-esac
+if [ "$1" ]; then
+    dbapi "$1"
+else
+    usage
+fi
 
